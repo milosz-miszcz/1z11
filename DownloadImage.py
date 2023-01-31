8163e9f99d65dbd5554db5e4a28a489bdd663b2f
miszcz <73019987+milosz-miszcz@users.noreply.github.com>
2023-01-31 22:31:25 +0100

    Update
    In the new version, the 'else' instructions have been added, which if the cell in column H is empty, it takes a distinctive image not related to the questions. The reason for adding these instructions is due to the fact that in the Unity engine, the images were numbered in a hierarchical manner and referring to a particular image was problematic. Now, the only problem is the amount of images loaded into the arrays at the start of the game. Before running the script, the paths must be changed if they do not match and retrieve the question sheets.

--- DownloadImage.py
+++ DownloadImage.py
@@ -1,7 +1,6 @@
 import requests
 import shutil
 import urllib
-
 from openpyxl import load_workbook
 
 wb = load_workbook(filename='BazaPytanS.xlsx')
@@ -13,11 +12,19 @@ for x in range(489):
     if sheet[text].value:
         print(x,sheet[text].value)
         image_url = sheet[text].value
-        filename = "UTK{}.png".format(x+1)
+        filename = "SOPER{}.png".format(x+1)
         def download_jpg(url, file_path, file_name):
             full_path = file_path + file_name
             urllib.request.urlretrieve(url, full_path)
         download_jpg(image_url, r"D:\Project\Image\Systemy/", filename)
+    else:
+        print(x, "https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg")
+        filename = "SOPER{}.png".format(x+1)
+        def download_jpg(url, file_path, file_name):
+            full_path = file_path + file_name
+            urllib.request.urlretrieve(url, full_path)
+        download_jpg("https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg", r"D:\Project\Image\Systemy/", filename)
+
 
 wb = load_workbook(filename='BazaPytanU.xlsx')
 sheet = wb['UTK']
@@ -31,7 +38,14 @@ for x in range(694):
         def download_jpg(url, file_path, file_name):
             full_path = file_path + file_name
             urllib.request.urlretrieve(url, full_path)
-        download_jpg(image_url, r"D:\Project\Image\Urządzenia/", filename)
+        download_jpg(image_url, r"D:\Project\Image\Urzadzenia/", filename)
+    else:
+        print(x, "https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg")
+        filename = "UTK{}.png".format(x+1)
+        def download_jpg(url, file_path, file_name):
+            full_path = file_path + file_name
+            urllib.request.urlretrieve(url, full_path)
+        download_jpg("https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg", r"D:\Project\Image\Urzadzenia/", filename)
 
 wb = load_workbook(filename='BazaPytanE.xlsx')
 sheet = wb['ELSK']
@@ -46,3 +60,10 @@ for x in range(1090):
             full_path = file_path + file_name
             urllib.request.urlretrieve(url, full_path)
         download_jpg(image_url, r"D:\Project\Image\Sieci/", filename)
+    else:
+        print(x, "https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg")
+        filename = "ELSK{}.png".format(x+1)
+        def download_jpg(url, file_path, file_name):
+            full_path = file_path + file_name
+            urllib.request.urlretrieve(url, full_path)
+        download_jpg("https://egzamin-informatyk.pl/assets/images/sprzet-systemy-ee08-inf02.jpg", r"D:\Project\Image\Sieci/", filename)
